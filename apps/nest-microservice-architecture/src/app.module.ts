ec1c7ba0243eb4efdcfa9d41e8c428df9ba52abf
Karol Zyśk <zysk.karol.pawel@gmail.com>
2023-03-23 09:03:51 +0100

    set up microservice architecture

--- apps/nest-microservice-architecture/src/app.module.ts
+++ apps/nest-microservice-architecture/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
