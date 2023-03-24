8486093a5db226699daa743a796d6864ad6e8f85
Karol Zyśk <zysk.karol.pawel@gmail.com>
2023-03-24 02:04:03 +0100

    Set up Docker and TypeORM for Postgres.

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,14 +1,32 @@
+import { UserEntity } from './user.entity';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
+import { dataSourceOptions } from './db/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: './.env',
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+
+      useFactory: () => ({
+        ...dataSourceOptions,
+        autoLoadEntities: true,
+        synchronize: true, // shouldn't be used in production
+      }),
+
+      inject: [ConfigService],
+    }),
+
+    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
